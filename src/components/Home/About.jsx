e8f3b3c7197ea97a49d33b87dae06876bc0e9ff4
Tanuja Chaudhary 12219564 <119854154+chaudharytanuja12219564@users.noreply.github.com>
2025-04-28 11:07:44 +0530

    Update About.jsx

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -33,8 +33,8 @@ const About = () => {
                   I enjoy tackling new challenges and continuously expanding my skillset.
                   <br />
                   <br />I am proficient in
-                    <b className="yellow"> Javascript, </b>
-                    as well as have knowledge in programming languages such as C++,HTML,Javascript
+                    <b className="yellow"> C++, </b>
+                    as well as have knowledge in programming languages such as HTML,Javascript 
                   <b className="yellow"> And Techstack like MERN</b>
                   <br />
                   <br />
